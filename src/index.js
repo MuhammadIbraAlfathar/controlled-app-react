8b353c2c49406b45a80da1813b7aa8acf93457de
Muhammad Ibra Alfathar <ibraalfathar22@gmail.com>
2022-12-07 11:29:43 +0700

    ubah

--- src/index.js
+++ src/index.js
@@ -1,17 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = {
+      name: "",
+      email: "",
+      gender: "Man",
+    };
+
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+    this.onGenderChangeHandler = this.onGenderChangeHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+
+  onNameChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value,
+      };
+    });
+  }
+
+  onEmailChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        email: event.target.value,
+      };
+    });
+  }
+
+  onGenderChangeHandler(event) {
+    this.setState((prevState) => {
+      return {
+        gender: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+
+    const message = `
+    Terimakasih Sudah Registrasi
+    Berikut Data Anda
+    AHAIIII
+
+    Name  : ${this.state.name}
+    Email : ${this.state.email}
+    Gender: ${this.state.gender}
+
+    `;
+
+    alert(message);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Register Form</h1>
+        <form onSubmit={this.onSubmitEventHandler}>
+          <table>
+            <tr>
+              <td>
+                <label for="name">Name:</label>
+              </td>
+              <td>
+                <input
+                  id="name"
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.onNameChangeEventHandler}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <label for="email">Email :</label>
+              </td>
+              <td>
+                <input
+                  id="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onEmailChangeEventHandler}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <label for="gender">Gender :</label>
+              </td>
+              <td>
+                <select
+                  id="gender"
+                  value={this.state.gender}
+                  onChange={this.onGenderChangeHandler}
+                >
+                  <option value="Man">Man</option>
+                  <option value="Woman">Woman</option>
+                </select>
+              </td>
+            </tr>
+
+            <button type="submit">Submit</button>
+          </table>
+        </form>
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<MyForm />);
